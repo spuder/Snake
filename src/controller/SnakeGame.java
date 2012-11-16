20aadd35076e780e6a7302da145a041243b61af7
Spencer Owen <bubbleboy450@gmail.com>
2012-11-16 13:32:04 -0700

    Added 2nd snake to test, works perfectly

--- src/controller/SnakeGame.java
+++ src/controller/SnakeGame.java
@@ -18,30 +18,46 @@ public class SnakeGame {
 		model.CubeAttributes.setyNumberOfRowsPerPanel(16);
 		model.CubeAttributes.setzNumberOfPanelsPerCube(16);
 		
-		ArrayList<Integer> aArray = new ArrayList<Integer>();
-		aArray.add(13);
-		aArray.add(14);
-		aArray.add(15);
+		ArrayList<Integer> snake1Position = new ArrayList<Integer>();
+		snake1Position.add(13);
+		snake1Position.add(14);
+		snake1Position.add(15);
+		
+		ArrayList<Integer> snake2Position = new ArrayList<Integer>();
+		snake2Position.add(4093);
+		snake2Position.add(4094);
+		snake2Position.add(4095);
 		
 		 
-		model.Snake aSnake = new model.Snake(255,3, aArray, 1000);
+		model.Snake aSnake = new model.Snake(255,3, snake1Position, 1000);
+		model.Snake aSnake2 = new model.Snake(115,3, snake2Position, 500);
 		
 		
 		Arena.createApple();
 //		Arena.createApple();
 		SnakeController aSnakeController = new SnakeController();
 		
-	while(true)
-	{
-		//Check to see if it is time to take action
-		if ( aSnakeController.checkTimeout(aSnake) == true)
+		while(true)
 		{
-			aSnakeController.advanceForward(aSnake);
-			aSnakeController.appleCheck(aSnake);
-//			aSnakeController.wallCheck(aSnake);
-//			aSnakeController.bodyCheck(aSnake);
+			
+			//Check to see if it is time to take action
+			if ( aSnakeController.checkTimeout(aSnake) == true)
+			{
+				aSnakeController.advanceForward(aSnake);
+				aSnakeController.appleCheck(aSnake);
+	//			aSnakeController.wallCheck(aSnake);
+	//			aSnakeController.bodyCheck(aSnake);
+			}
+			
+			//Check to see if it is time to take action
+			if ( aSnakeController.checkTimeout(aSnake2) == true)
+			{
+				aSnakeController.advanceForward(aSnake2);
+				aSnakeController.appleCheck(aSnake2);
+//					aSnakeController.wallCheck(aSnake2);
+//					aSnakeController.bodyCheck(aSnake2);
+			}
 		}
-	}
 		
 		
 		
